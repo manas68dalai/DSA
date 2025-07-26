25737040051a8743dd8be80318dc835c775637a6
Manas Dalai <manasdalai2004@gmail.com>
2025-07-27 00:20:12 +0530

    second pattern

---
+++ Pattern Problem/2nd Pattern.py
@@ -0,0 +1,14 @@
+"""
+* 
+* *
+* * *
+"""
+
+## Code
+
+n = int(input("Enter the no. of Lines: "))
+for i in range(n+1):
+        for j in range(i):
+            print("*", end = " ")
+        print()
+pass
