5e77a3feef96d490b2d8ec345032f55b88bbc031
Manas Dalai <manasdalai2004@gmail.com>
2025-07-27 01:19:54 +0530

    eighth pattern

---
+++ Pattern Problem/8th Pattern.py
@@ -0,0 +1,16 @@
+"""
+*****
+ ***
+  *
+"""
+
+## Code
+
+n = int(input("Enter the no. of Lines: "))
+for i in range(n):
+    for j in range(i):
+        print(" ", end = "")
+    for j in range((2*n)-((2*i)+1)):
+        print("*", end = "")
+    print()
+pass
