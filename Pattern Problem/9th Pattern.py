aa78564beb44cb9228d1e8572658505999d9edba
Manas Dalai <manasdalai2004@gmail.com>
2025-07-27 01:31:01 +0530

    ninth pattern

---
+++ Pattern Problem/9th Pattern.py
@@ -0,0 +1,25 @@
+"""
+  *
+ ***
+*****
+*****
+ ***
+  *
+"""
+
+## Code
+
+n = int(input("Enter the no. of Lines: "))
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ", end = "")
+    for j in range((2*i)+1):
+        print("*", end = "")
+    print()
+for i in range(n):
+    for j in range(i):
+        print(" ", end = "")
+    for j in range((2*n)-((2*i)+1)):
+        print("*", end = "")
+    print()
+pass
