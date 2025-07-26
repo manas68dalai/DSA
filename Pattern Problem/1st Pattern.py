7e25440d22797fc5943f0de8b9506bc7a39f93c5
Manas Dalai <manasdalai2004@gmail.com>
2025-07-27 00:14:33 +0530

    first pattern

---
+++ Pattern Problem/1st Pattern.py
@@ -0,0 +1,15 @@
+"""
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+"""
+
+## Code
+
+n = int(input("Enter the no. of lines: "))
+for i in range(n):
+        for j in range(n):
+            print("*",end= " ")
+        print()
+pass
