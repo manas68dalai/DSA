2551fc6a0feda154d0194f147bad416909d59848
Manas Dalai <manasdalai2004@gmail.com>
2025-07-27 00:57:27 +0530

    fifth pattern

---
+++ Pattern Problem/5th Pattern.py
@@ -0,0 +1,16 @@
+"""
+* * * * *
+* * * *
+* * *
+* * 
+*
+"""
+
+## Code
+
+n = int(input("Ebter the no. of Lines: "))
+for i in range(n):
+    for j in range(n-i):
+        print("*", end = " ")
+    print()
+pass
