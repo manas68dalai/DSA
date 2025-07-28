3916e3853ad662622d8696658221df312f6c521d
Manas Dalai <manasdalai2004@gmail.com>
2025-07-29 02:09:10 +0530

    twientyth pattern

---
+++ Pattern Problem/20th Pattern.py
@@ -0,0 +1,28 @@
+"""
+*         *
+* *     * *
+* * * * * *
+* *     * *
+*         *
+"""
+
+## Code
+
+n = int(input("Enter the no. of Lines: "))
+for i in range(n-1):
+    for j in range(i+1):
+        print("*", end = " ")
+    for j in range(4 - (2*i)):
+        print(" ", end = " ")
+    for j in range(i+1):
+        print("*",end = " ")
+    print()
+for i in range(n):
+    for j in range(n-i):
+        print("*", end = " ")
+    for j in range(2*i):
+        print(" ", end = " ")
+    for j in range(n-i):
+        print("*", end = " ")
+    print()
+pass
